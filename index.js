88227b768df66cfb5dc580b377e1a4a30eafcee4
Shami <shamitomita@gmail.com>
2021-12-13 10:35:23 -0500

    commit tree 73a1841367ad028e385dfbabd62e85c81df97318

--- index.js
+++ index.js
@@ -13,5 +13,9 @@ const tutorials = [
 
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
-  return tutorials
+  const newTutorials = tutorials.map(tutorial => tutorial
+    .split(' ')
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(' '));
+  return newTutorials
 }
